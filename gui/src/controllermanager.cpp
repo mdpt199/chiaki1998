708cd1a25ca13faff5fbc2b1e47626540b200656
mdpt199 <maicon.jpdistribuicao@gmail.com>
2026-02-19 21:54:33 -0300

    Update controllermanager.cpp

--- gui/src/controllermanager.cpp
+++ gui/src/controllermanager.cpp
@@ -11,7 +11,7 @@
 #include <SDL.h>
 #endif
 
-// --- Estrutura para o Anti-Recoil ---
+// --- Estrutura e Variáveis Globais para o Anti-Recoil ---
 struct RecoilProfile {
     int vertical = 0;   
     int horizontal = 0; 
@@ -20,29 +20,17 @@ struct RecoilProfile {
 
 static RecoilProfile currentRecoil; 
 
-// Funções auxiliares internas (estáticas para não precisarem estar no .h)
-static void InternalLoadSettings() {
-    QSettings s;
-    currentRecoil.vertical = s.value("recoil/vertical", 0).toInt();
-    currentRecoil.horizontal = s.value("recoil/horizontal", 0).toInt();
-    currentRecoil.enabled = s.value("recoil/enabled", false).toBool();
-}
-
-static void InternalSaveSettings() {
-    QSettings s;
-    s.setValue("recoil/vertical", currentRecoil.vertical);
-    s.setValue("recoil/horizontal", currentRecoil.horizontal);
-    s.setValue("recoil/enabled", currentRecoil.enabled);
-}
-
 static QSet<QString> chiaki_motion_controller_guids({
 	"03000000341a00003608000011010000",
-    // ... (mantenha sua lista de GUIDs original aqui)
+    "050000004c050000cc090000ffff3f00",
+    "3J5TqBf8fb7w58vA6mJ7KBtqLjmtQaD3QY",
 	"030000008f0e00001431000000000000",
 });
 
 static ControllerManager *instance = nullptr;
 #define UPDATE_INTERVAL_MS 4
+
+// --- Implementação do ControllerManager ---
 
 ControllerManager *ControllerManager::GetInstance()
 {
@@ -66,7 +54,13 @@ ControllerManager::ControllerManager(QObject *parent)
 	connect(timer, &QTimer::timeout, this, &ControllerManager::HandleEvents);
 	timer->start(UPDATE_INTERVAL_MS);
 #endif
-    InternalLoadSettings(); // Agora chama a função estática interna
+    
+    // Carrega configurações via QSettings diretamente
+    QSettings s;
+    currentRecoil.vertical = s.value("recoil/vertical", 0).toInt();
+    currentRecoil.horizontal = s.value("recoil/horizontal", 0).toInt();
+    currentRecoil.enabled = s.value("recoil/enabled", false).toBool();
+
 	UpdateAvailableControllers();
 }
 
@@ -77,7 +71,108 @@ ControllerManager::~ControllerManager()
 #endif
 }
 
-// ... (Mantenha UpdateAvailableControllers, HandleEvents, ControllerEvent, GetAvailableControllers, OpenController, ControllerClosed, Controller e ~Controller iguais ao original)
+void ControllerManager::UpdateAvailableControllers()
+{
+#ifdef CHIAKI_GUI_ENABLE_SDL_GAMECONTROLLER
+	QSet<SDL_JoystickID> current_controllers;
+	for(int i=0; i<SDL_NumJoysticks(); i++)
+	{
+		if(!SDL_IsGameController(i))
+			continue;
+
+		current_controllers.insert(SDL_JoystickGetDeviceInstanceID(i));
+	}
+
+	if(current_controllers != available_controllers)
+	{
+		available_controllers = current_controllers;
+		emit AvailableControllersUpdated();
+	}
+#endif
+}
+
+void ControllerManager::HandleEvents()
+{
+#ifdef CHIAKI_GUI_ENABLE_SDL_GAMECONTROLLER
+	SDL_Event event;
+	while(SDL_PollEvent(&event))
+	{
+		switch(event.type)
+		{
+			case SDL_JOYDEVICEADDED:
+			case SDL_JOYDEVICEREMOVED:
+				UpdateAvailableControllers();
+				break;
+			case SDL_CONTROLLERBUTTONUP:
+			case SDL_CONTROLLERBUTTONDOWN:
+				ControllerEvent(event.cbutton.which);
+				break;
+			case SDL_CONTROLLERAXISMOTION:
+				ControllerEvent(event.caxis.which);
+				break;
+		}
+	}
+#endif
+}
+
+void ControllerManager::ControllerEvent(int device_id)
+{
+	if(!open_controllers.contains(device_id))
+		return;
+	open_controllers[device_id]->UpdateState();
+}
+
+QSet<int> ControllerManager::GetAvailableControllers()
+{
+#ifdef CHIAKI_GUI_ENABLE_SDL_GAMECONTROLLER
+	return available_controllers;
+#else
+	return {};
+#endif
+}
+
+Controller *ControllerManager::OpenController(int device_id)
+{
+	if(open_controllers.contains(device_id))
+		return nullptr;
+	auto controller = new Controller(device_id, this);
+	open_controllers[device_id] = controller;
+	return controller;
+}
+
+void ControllerManager::ControllerClosed(Controller *controller)
+{
+	open_controllers.remove(controller->GetDeviceID());
+}
+
+// --- Implementação do Controller ---
+
+Controller::Controller(int device_id, ControllerManager *manager) : QObject(manager)
+{
+	this->id = device_id;
+	this->manager = manager;
+
+#ifdef CHIAKI_GUI_ENABLE_SDL_GAMECONTROLLER
+	controller = nullptr;
+	for(int i=0; i<SDL_NumJoysticks(); i++)
+	{
+		if(SDL_JoystickGetDeviceInstanceID(i) == device_id)
+		{
+			controller = SDL_GameControllerOpen(i);
+			break;
+		}
+	}
+#endif
+}
+
+Controller::~Controller()
+{
+#ifdef CHIAKI_GUI_ENABLE_SDL_GAMECONTROLLER
+	if(controller)
+		SDL_GameControllerClose(controller);
+#endif
+	manager->ControllerClosed(this);
+}
 
 void Controller::UpdateState()
 {
@@ -95,11 +190,7 @@ bool Controller::IsConnected()
 
 int Controller::GetDeviceID()
 {
-#ifdef CHIAKI_GUI_ENABLE_SDL_GAMECONTROLLER
 	return id;
-#else
-	return -1;
-#endif
 }
 
 QString Controller::GetName()
@@ -108,10 +199,7 @@ QString Controller::GetName()
 	if(!controller)
 		return QString();
 	SDL_Joystick *js = SDL_GameControllerGetJoystick(controller);
-	SDL_JoystickGUID guid = SDL_JoystickGetGUID(js);
-	char guid_str[256];
-	SDL_JoystickGetGUIDString(guid, guid_str, sizeof(guid_str));
-	return QString("%1 (%2)").arg(SDL_JoystickName(js), guid_str);
+	return QString(SDL_JoystickName(js));
 #else
 	return QString();
 #endif
@@ -125,6 +213,7 @@ ChiakiControllerState Controller::GetState()
 	if(!controller)
 		return state;
 
+    // Mapeamento original
 	state.buttons |= SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_A) ? CHIAKI_CONTROLLER_BUTTON_CROSS : 0;
 	state.buttons |= SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_B) ? CHIAKI_CONTROLLER_BUTTON_MOON : 0;
 	state.buttons |= SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_X) ? CHIAKI_CONTROLLER_BUTTON_BOX : 0;
@@ -145,7 +234,7 @@ ChiakiControllerState Controller::GetState()
     int16_t rx = SDL_GameControllerGetAxis(controller, SDL_CONTROLLER_AXIS_RIGHTX);
     int16_t ry = SDL_GameControllerGetAxis(controller, SDL_CONTROLLER_AXIS_RIGHTY);
 
-    // Lógica de Anti-Recoil aplicada apenas quando R2 é pressionado
+    // Lógica de Anti-Recoil (Compensação)
     if (currentRecoil.enabled && r2_raw > 1000) {
         int32_t comp_y = ry + currentRecoil.vertical;
         int32_t comp_x = rx + currentRecoil.horizontal;
@@ -159,7 +248,6 @@ ChiakiControllerState Controller::GetState()
 	state.left_y = SDL_GameControllerGetAxis(controller, SDL_CONTROLLER_AXIS_LEFTY);
 	state.right_x = rx;
 	state.right_y = ry;
-
 #endif
 	return state;
 }
@@ -167,8 +255,7 @@ ChiakiControllerState Controller::GetState()
 void Controller::SetRumble(uint8_t left, uint8_t right)
 {
 #ifdef CHIAKI_GUI_ENABLE_SDL_GAMECONTROLLER
-	if(!controller)
-		return;
+	if(!controller) return;
 	SDL_GameControllerRumble(controller, (uint16_t)left << 8, (uint16_t)right << 8, 5000);
 #endif
 }
